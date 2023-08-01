88f22390967d1a7d3ce0a7b702f1f3f9fa549fdb
mattbaysinger <21045933+mattbaysinger@users.noreply.github.com>
2023-08-01 17:11:09 -0600

    implemented dark mode toggle
    respects system settings but can be overridden

---
+++ script.js
@@ -0,0 +1,64 @@
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: Dark)");
+const themeToggle = document.getElementById('theme-toggle')
+
+
+// Match the icon to the theme. This only works on page reload.
+if (prefersDarkScheme.matches) {
+   
+   themeToggle.innerHTML = `<span class="material-symbols-outlined">
+     dark_mode
+    </span>`
+    document.body.classList.toggle("dark-theme")
+ }
+
+ else {
+       themeToggle.innerHTML = `<span class="material-symbols-outlined">
+        light_mode
+        </span>`
+        document.body.classList.toggle("light-theme")
+     }
+
+
+// Listen for a click on the button 
+themeToggle.addEventListener("click", function() {
+
+    if (prefersDarkScheme.matches) {
+
+        if (document.body.classList.contains("light-theme")){
+            themeToggle.innerHTML = `<span class="material-symbols-outlined">
+             dark_mode
+             </span>`
+             document.body.classList.replace("light-theme", "dark-theme")
+            
+        }
+        
+        else {
+            themeToggle.innerHTML = `<span class="material-symbols-outlined">
+            light_mode
+           </span>`
+           document.body.classList.replace("dark-theme", "light-theme")
+        }
+       
+      }
+     
+      else {
+
+        if(document.body.classList.contains("dark-theme")){
+            themeToggle.innerHTML = `<span class="material-symbols-outlined">
+             light_mode
+             </span>`
+             document.body.classList.replace("dark-theme", "light-theme")
+        }
+            else {
+                themeToggle.innerHTML = `<span class="material-symbols-outlined">
+                dark_mode
+                </span>`
+                document.body.classList.replace("light-theme", "dark-theme")
+            }
+          
+          }
+
+})
+
+
+
