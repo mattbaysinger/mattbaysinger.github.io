472ec5eb70d7dca4b87af41570e8e736afcf839f
mattbaysinger <21045933+mattbaysinger@users.noreply.github.com>
2024-09-24 12:11:53 -0600

    flipped theme icons

--- script.js
+++ script.js
@@ -1,73 +1,57 @@
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: Dark)");
-const themeToggle = document.getElementById('theme-toggle')
-
-
-
+const themeToggle = document.getElementById("theme-toggle");
 
 // Checks theme on inial page load and sets theme and icon class
 if (prefersDarkScheme.matches) {
-   
-   themeToggle.innerHTML = `<span class="material-symbols-outlined">
-     dark_mode
-    </span>`
-    document.body.classList.toggle("dark-theme");
-   
- }
-
- else {
-       themeToggle.innerHTML = `<span class="material-symbols-outlined">
-        light_mode
-        </span>`
-        document.body.classList.toggle("light-theme");
-  
-     }
-
-// listen for change to system theme and update accordingly
- window.matchMedia('(prefers-color-scheme: dark)')
-     .addEventListener('change',({ matches }) => {
- if (matches) {
-   toggleDark()
- } else {
-  toggleLight()
- }
-})
-
-// Listen for a click on the button 
-themeToggle.addEventListener("click", function() {
-
-    if (prefersDarkScheme.matches) {
-
-        if (document.body.classList.contains("light-theme")){
-           toggleDark()           
-         }        
-        else {
-            toggleLight()
-         }}
-     
-    else {
-
-        if(document.body.classList.contains("dark-theme")){
-            toggleLight()
-        }
-            else {
-              toggleDark()
-            }}
-})
-
-
-function toggleLight(){
   themeToggle.innerHTML = `<span class="material-symbols-outlined">
-             light_mode
-             </span>`
-             document.body.classList.replace("dark-theme", "light-theme")
-      
-          
+     light_mode
+    </span>`;
+  document.body.classList.toggle("dark-theme");
+} else {
+  themeToggle.innerHTML = `<span class="material-symbols-outlined">
+        dark_mode
+        </span>`;
+  document.body.classList.toggle("light-theme");
 }
 
-function toggleDark(){
+// listen for change to system theme and update accordingly
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches }) => {
+    if (matches) {
+      toggleDark();
+    } else {
+      toggleLight();
+    }
+  });
+
+// Listen for a click on the button
+themeToggle.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    if (document.body.classList.contains("light-theme")) {
+      toggleDark();
+    } else {
+      toggleLight();
+    }
+  } else {
+    if (document.body.classList.contains("dark-theme")) {
+      toggleLight();
+    } else {
+      toggleDark();
+    }
+  }
+});
+
+function toggleLight() {
   themeToggle.innerHTML = `<span class="material-symbols-outlined">
-  dark_mode
-  </span>`
-  document.body.classList.replace("light-theme", "dark-theme")
-  
+             dark_mode
+             </span>`;
+  document.body.classList.replace("dark-theme", "light-theme");
+}
+
+function toggleDark() {
+  themeToggle.innerHTML = `<span class="material-symbols-outlined">
+  light_mode
+  </span>`;
+  document.body.classList.replace("light-theme", "dark-theme");
 }
